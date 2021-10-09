15e444dc3eaccacbe15699bc9d71256d716640d9
Giedrius Jocius <giedrius393@gmail.com>
2021-10-09 23:46:41 +0300

    add initial redux setup

--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -1,9 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import { test, expect } from '@testing-library/jest-dom';
-import App from '../App';
+// TODO: Add tests
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// import { render, screen } from '@testing-library/react';
+// import { test, expect } from '@testing-library/jest-dom';
+// import App from '../App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
---
+++ src/state/images/reducer.ts
@@ -0,0 +1,14 @@
+const initialState = {
+  imagesList: null,
+  isLoading: false,
+};
+
+const imagesReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+};
+
+export default imagesReducer;
