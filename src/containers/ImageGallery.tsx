e9fe2fe8160574c1f75686b9119ca8e2bc18cbba
Giedrius Jocius <giedrius393@gmail.com>
2021-10-10 21:46:46 +0300

    Fix image list column count

--- src/containers/ImageGallery.tsx
+++ src/containers/ImageGallery.tsx
@@ -5,42 +5,42 @@ import { ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
 import { loadImages } from '../state/images/actions';
 import { useAppSelector } from '../state/hooks';
 
+const getColsByWidth = (): number => {
+  const width = window.innerWidth;
+
+  if (width < 400) return 1;
+  if (width < 600) return 2;
+  if (width < 1100) return 3;
+
+  return 5;
+};
+
 function ImageGallery(): JSX.Element {
   const dispatch = useDispatch();
+  const [imageListCols, setListCols] = useState(3);
   const { imagesList, isLoading } = useAppSelector((state) => state.images);
-  const [listCols, setListCols] = useState(3);
 
-  const setListColsByWidth = (width: number) => {
-    if (width < 400) {
-      setListCols(1);
-    } else if (width < 600) {
-      setListCols(2);
-    } else if (width < 1100) {
-      setListCols(3);
-    } else {
-      setListCols(5);
-    }
-  };
-  const handleResize = () => setListColsByWidth(window.innerWidth);
+  const setImageListCols = () => setListCols(getColsByWidth());
 
   useEffect(() => {
     dispatch(loadImages);
-    setListColsByWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
+    setImageListCols();
+    window.addEventListener('resize', setImageListCols);
 
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', setImageListCols);
   }, []);
 
   if (isLoading || !imagesList) return <p>Loading</p>;
 
   return (
     <>
-      <ImageList cols={listCols} variant='masonry'>
+      <ImageList cols={imageListCols} variant='masonry'>
         {imagesList.map((image) => (
           <ImageListItem key={image.id}>
             <img
               src={image.url}
               alt={image.description}
+              loading='lazy'
             />
             <ImageListItemBar
               title={image.description}
